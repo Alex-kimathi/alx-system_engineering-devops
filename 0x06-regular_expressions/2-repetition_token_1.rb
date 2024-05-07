dcc9cc0db931b8e18d5c338253f8920dfd968ef5
Alex-kimathi <alex2kimathi@gmail.com>
2024-05-07 21:02:44 +0000

    check match for hbtn or htn

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# create a Ruby script that accepts one argument and
+# pass it to a regular expression matching method
+# the match is 'hbtn' or htn
+#
+puts ARGV[0].scan(/h[bt]n$/).join
