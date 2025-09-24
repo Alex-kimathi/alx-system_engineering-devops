0701ff10b420fd2ed618014a6ffd6ecc7a952e91
Alex Kimathi <alex2kimathi@gmail.com>
2025-09-25 00:29:17 +0300

    creates a file in /tmp/school

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This Puppet manifest creates the file /tmp/school with specific permissions, ownership, and content
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => "I love Puppet\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
+
